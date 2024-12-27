2ca39e6c60047c60af7b26f1b05c6fd67d83bce6
Govind <govind.beena@workz.com>
2024-12-27 10:33:22 +0400

    update the Home

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,7 +10,43 @@ const LoginForm = () => {
 
   return (
     <div className='login'>
-      
+      <form onSubmit={handleSubmit(onsubmit)}>
+        <h1>Login</h1>
+        <div className='ui divider'></div>
+        <div className='ui form'>
+        <div className="field">
+            <label htmlFor="">Username</label>
+            <input 
+               type='text'
+               name='username'
+               placeholder='Username'
+               ref={register({required : "Username is required " })}
+            />
+        </div>
+        <p>{errors.username?.message}</p>
+        <div className='field'>
+            <label>Password</label>
+            <input
+            type="password"
+              name="password"
+              placeholder="Password"
+              ref={register({
+                required: "Password is required",
+                minLength: {
+                  value: 4,
+                  message: "Password must be more than 4 characters",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "Password cannot exceed more than 10 characters",
+                },
+              })}
+             />
+        </div>
+        <p>{errors.password?.message}</p>
+        <button className='fluid ui button blue'>Submit</button>
+        </div>
+      </form>
     </div>
   )
 }
